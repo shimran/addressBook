e700783b87d4c53516bfe3b9704f62cc7e3ceebe
Alister Fenix & Shimran George <student@epicodus.com>
2015-02-16 11:40:10 -0800

    Add key logic for storing contact information and display

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
-var addressBook = (function() {
+$(document).ready(function(){
+  $('form#new-contact').submit(function(event) {
+    event.preventDefault();
+
+    var inputFirstName = $("input#new-first-name").val();
+    var inputLastName = $("input#new-last-name").val();
+    var inputAddress = $("input#new-address").val();
+    var inputTelephone = $("input#new-telephone").val();
+    var newContact = { firstName: inputFirstName,
+                        lastName: inputLastName,
+                        address: inputAddress,
+                        telephone: inputTelephone };
 
 
-});
+  $("ul#contact").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span> </li>");
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input#new-address").val("");
+  $("input#new-telephone").val("");
+
+  // Append id=contact to the form, and then use a span class =contact to store information for each link
+
+$(".contact").last().click(function() {
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+  $(".address").text(newContact.address);
+  $(".telephone").text(newContact.telephone);
+
+  });
+
+  });
+ });
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,8 @@
 describe('addressBook', function(){
 
+  it('Will add a first name.', function{
+    person.firstName
+    expect(person.firstName).to.equal("Shimran")    
+  })
 
 });
